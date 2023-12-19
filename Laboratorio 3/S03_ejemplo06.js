cdee2fa8636c94e7831357cecbf4158b0ba5702e
Zelvalla <sabrina.zambrano@tecsup.edu.pe>
2023-12-18 23:51:22 -0500

    Labs finished

---
+++ Laboratorio 3/S03_ejemplo06.js
@@ -0,0 +1,11 @@
+let nombreCompleto = "Juan Perez";
+let nombrecompleto = "Carlos Lara";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+let a1nombreCompleeto;
+let _nombreCompleto;
+let $nombreCompleto;
+//let 1nombreCompleto;
+
+let ruptura = 10;
---
+++ Laboratorio 3/S03_ejemplo24.js
@@ -0,0 +1,8 @@
+miFuncion(4, 2);
+//Declarando la funcion
+function miFuncion(a, b){
+    console.log("Suma: " + (a + b));
+}
+
+//Llamando la funcion
+miFuncion(2, 3);
